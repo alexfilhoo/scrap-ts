4a0241832e4102ec76db77c476ba38ebb09ddc5e
Alexandro Filho <alexandro.filho@pullse.online>
2024-06-20 12:00:06 -0300

    feat: type and click puppeteer

--- index.ts
+++ index.ts
@@ -1,54 +1,30 @@
-const puppeteer = require("puppeteer");
-const fs = require("fs");
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 import { Browser } from "puppeteer";
 
-const url = "https://books.toscrape.com/";
+puppeteer.use(StealthPlugin());
+
+const {executablePath} = require('puppeteer');
+
+const url = "https://www.facebook.com/";
+
+function sleep(timeout: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeout);
+  });
+}
 
 const main = async () => {
-  const browser: Browser = await puppeteer.launch({ headless: false });
+  const browser: Browser = await puppeteer.launch({ headless: false, executablePath: executablePath()});
   const page = await browser.newPage();
   await page.goto(url);
+  await page.type('[name="email"]', 'filho@puppeteer.com');
+  await page.type('[name="pass"]', 'puppeteeruso');
+  await page.click('[name="login"]');
 
-  const booksData = await page.evaluate((url) => {
-    const convertPrice = (price: string) => {
-      const priceStr = price.replace("£", "");
-      return parseFloat(priceStr);
-    };
-
-    const convertRating = (rating: string) => {
-      switch (rating) {
-        case "One":
-          return 1;
-        case "Two":
-          return 2;
-        case "Three":
-          return 3;
-        case "Four":
-          return 4;
-        case "Five":
-          return 5;
-        default:
-          return 0;
-      }
-    };
-
-    const bookPods = Array.from(document.querySelectorAll(".product_pod"));
-    const data = bookPods.map((book: any) => ({
-      title: book.querySelector("h3 a").getAttribute("title"),
-      price: convertPrice(book.querySelector(".price_color").innerText),
-      imgSrc: url + book.querySelector("img").getAttribute("src"),
-      rating: convertRating(book.querySelector(".star-rating").classList[1]),
-    }));
-    return data;
-  }, url);
-  console.log(booksData);
+  await sleep(30000);
 
   await browser.close();
-
-  fs.writeFile("books.json", JSON.stringify(booksData), (err: any) => {
-    if (err) throw err;
-    console.log("The file has been saved!");
-  });
 };
 
 main();
